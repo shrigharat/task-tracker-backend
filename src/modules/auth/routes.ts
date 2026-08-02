5180677109b6ead2626d227278029029d5d0f351
shrishail2000 <shrishail@grexa.ai>
2026-08-03 00:04:45 +0530

    added refresh access token endpoint

--- src/modules/auth/routes.ts
+++ src/modules/auth/routes.ts
@@ -1,14 +1,10 @@
 import { Hono } from 'hono'
-import {
-  loginUser,
-  registerUser,
-  validateUserLoginRequest,
-  validateUserRegistrationRequest,
-} from './controller'
+import { loginUser, refreshAccessToken, registerUser } from './controller'
 
 const authRouter = new Hono()
 
 authRouter.post('/register', registerUser)
 authRouter.post('/login', loginUser)
+authRouter.post('/refresh', refreshAccessToken)
 
 export { authRouter }
